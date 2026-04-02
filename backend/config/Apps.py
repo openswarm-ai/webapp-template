4165cb65299ced01d02b261e10b942a099bc9625
haikdc <haikdecie@gmail.com>
2026-04-02 10:20:10 -0700

    [Haik]: added in swarm-debug

--- backend/config/Apps.py
+++ backend/config/Apps.py
@@ -1,36 +1,33 @@
-import logging
 from fastapi import FastAPI, APIRouter
 from uuid import uuid4
 from typing import List
 from contextlib import asynccontextmanager
 from contextlib import AsyncExitStack
 from typing import Callable
-
-log = logging.getLogger(__name__)
-
+from swarm_debug import debug
 
 class SubApp:
     def __init__(self, name:str, lifespan:Callable):
-        log.debug("SubApp.__init__ START: %s", name)
+        debug("SubApp.__init__ START: %s", name)
         self.id = uuid4()
         self.name = name
         self.prefix = f"/api/{name}"
         self.lifespan = lifespan
         self.router = APIRouter()
-        log.debug("SubApp.__init__ END")
+        debug("SubApp.__init__ END")
     
     def __str__(self):
         return f"SubApp(name={self.name}, prefix={self.prefix}, id={self.id})"
 
 class MainApp:
     def __init__(self, sub_apps: List[SubApp]):
-        log.debug("MainApp.__init__ START")
+        debug("MainApp.__init__ START")
         
         @asynccontextmanager
         async def lifespan(app: FastAPI):
             async with AsyncExitStack() as stack:
                 for sub_app in sub_apps:
-                    log.debug("Starting lifespan for sub_app: %s", sub_app.name)
+                    debug("Starting lifespan for sub_app: %s", sub_app.name)
                     await stack.enter_async_context(sub_app.lifespan())
                 print("\nCheck out the API docs at: http://127.0.0.1:8324/docs\n")
                 yield
@@ -43,4 +40,4 @@ class MainApp:
                 prefix=sub_app.prefix,
                 tags=[sub_app.name]
             )
-        log.debug("MainApp.__init__ END")
+        debug("MainApp.__init__ END")
